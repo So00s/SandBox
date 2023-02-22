6125cbe646484591ae4427ae397f0001636c5ffb
So00s <105886252+So00s@users.noreply.github.com>
2023-02-22 23:31:42 +0300

    Update Classes.cpp

--- Classes.cpp
+++ Classes.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string.h>
 using namespace std;
 
 class Class1
@@ -10,17 +10,17 @@ private:
     int age;
     double weight;
 public:
-    void set(string name, string subname, int age, double weight)
+    void set(string n, string s, int a, double w)
     {
-        name = name;
-        subname = subname;
-        age = age;
-        weight = weight;
+        name = n;
+        subname = s;
+        age = a;
+        weight = w;
     }
 
     void out()
     {
-        cout << name << " " << subname << " " << age << " " << weight << endl;
+        cout << name << endl << subname << endl << age << endl << weight << endl;
     } 
 
 };
@@ -28,13 +28,18 @@ public:
 int main()
 {
     string name = "Sanya";
+    string name2 = "Alex";
     string subname = "Volkov";
+    string subname2 = "AUF";
     int age = 21;
+    int age2 = 13;
     double weight = 198.5;
-
+    double weight2 = 156;
     Class1 parameters;
     Class1 parameters2;
-    parameters.set(name, subname, age, weight);
+    parameters.set(name,subname,age,weight);
     parameters.out();
+    parameters2.set(name2, subname2, age2, weight2);
+    parameters2.out();
     return 0;
 }
