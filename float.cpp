25497f2a21795e392b52dac4c91927cfb0dc6ed1
So00s <105886252+So00s@users.noreply.github.com>
2023-03-19 18:38:24 +0300

    Update float.cpp

--- float.cpp
+++ float.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
 
@@ -8,23 +8,24 @@ struct float_as_struct
 {
     unsigned int fraction : 23; // мантисса
     unsigned int exponent : 8; // порядок
-    unsigned int sign : 1; // бит знака
+    unsigned int sign : 1; // бит знака 
 };
 
 
 void dec_to_bin(unsigned int decnum)
 {
-    int binnum = 0;
-    string temp = "";
+    vector <int> temp;
     // Написать простой алгоритм по переводу из 10 в 2, все числа если что целые, простой перевод в прямой код
     do 
     {
-        temp = temp + to_string(decnum % 2);
+        temp.insert(temp.begin(), decnum % 2);
         decnum = decnum / 2;
     } while (decnum > 0);
     
-    reverse(temp.begin(), temp.end());
-    cout << temp << endl;
+    for(int i = 0; i < temp.size(); i++)
+    {
+        cout << temp[i];
+    }    
 }
 
 
@@ -41,9 +42,15 @@ int main()
     cout << "Мантисса в десятичной: " << t.fraction << endl; // Выводится значение мантиссы в 10
     cout << "Порядок в десятичной: " << t.exponent << endl; // Выводится значение порядка в 10
     cout << "---------------------------------------------" << endl;
-    dec_to_bin(t.sign);
+    cout << "Знак в двоичной: ";
+    dec_to_bin(t.sign); // Не работает потому что надо написать функцию
+    cout << endl;
+    cout << "Мантисса в двоичной: ";
     dec_to_bin(t.fraction);
+    cout << endl; // Не работает потому что надо написать функцию
+    cout << "Порядок в двоичной: ";
     dec_to_bin(t.exponent);
+    cout << endl; // Не работает потому что надо написать функцию
     //---------------------------------------------
 
     return 0;
